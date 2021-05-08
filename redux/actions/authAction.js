80a0b34ce3fa82afe7103338357d42c091601ad7
Max de Vries <max.devries.1995@gmail.com>
2021-05-08 16:24:28 +0200

    Add post request to the server for login and register and pass the data to the reducer

--- redux/actions/authAction.js
+++ redux/actions/authAction.js
@@ -1,3 +1,5 @@
+import { BASE_URL } from '../../constants/api';
+
 export const REGISTER_USER_SUCCESS = 'REGISTER_USER_SUCCESS';
 export const REGISTER_USER_FAIL = 'REGISTER_USER_FAIL';
 export const LOGIN_USER_SUCCESS = 'LOGIN_USER_SUCCESS';
@@ -7,10 +9,27 @@ export const registerUser = (authData) => {
   const { fullName, email, password } = authData;
 
   return async dispatch => {
+    const result = await fetch(`${BASE_URL}/api/users/register`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        fullName,
+        email,
+        password,
+      }),
+    })
+
+    const resultData = await result.json();
+    const actionType = resultData.success ? REGISTER_USER_SUCCESS : REGISTER_USER_FAIL;
+
     dispatch({
-      type: REGISTER_USER_SUCCESS,
-      payload: 1,
+      type: actionType,
+      payload: resultData,
     });
+
+    return resultData;
   }
 }
 
@@ -18,9 +37,25 @@ export const loginUser = (authData) => {
   const { email, password } = authData;
 
   return async dispatch => {
+    const result = await fetch(`${BASE_URL}/api/users/login`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+    })
+
+    const resultData = await result.json();
+    const actionType = resultData.success ? LOGIN_USER_SUCCESS : LOGIN_USER_FAIL;
+
     dispatch({
-      type: LOGIN_USER_SUCCESS,
-      payload: 1,
+      type: actionType,
+      payload: resultData,
     });
+
+    return resultData;
   }
 }
