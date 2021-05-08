ec4e3f08020ae50cd95f6fe9b184dab875b2c635
Max de Vries <max.devries.1995@gmail.com>
2021-05-08 16:25:16 +0200

    Update the homescreen with the information about the logged in user

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,47 @@
-import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, View, Text, Button } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import jwtDecode from 'jwt-decode';
 
 export default function HomeScreen(props) {
+  const [fullName, setFullName] = useState('');
+  const [email, setEmail] = useState('');
+
+  const loadProfile = async () => {
+    const token = await AsyncStorage.getItem('token');
+    if (!token) props.navigation.navigate('Login');
+
+    const decoded = jwtDecode(token);
+    setFullName(decoded.fullName);
+    setEmail(decoded.email);
+  }
+
+  const logout = (props) => {
+    AsyncStorage.removeItem('token')
+      .then(() => {
+        props.navigation.replace('Login');
+      })
+      .catch(err => console.log(err));
+  }
+
+  useEffect(() => {
+    loadProfile();
+  });
+
   return (
     <View>
-      <Text>home screen</Text>
+      <View>
+        <Text>Welcome { fullName }</Text>
+      </View>
+      <View>
+        <Text>Your email: { email }</Text>
+      </View>
+      <View>
+        <Button
+          title="Logout"
+          onPress={() => logout(props)}
+        />
+      </View>
     </View>
   );
 }
